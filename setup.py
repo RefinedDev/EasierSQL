8a196c48b5fdb4955bf49bca39eab11f7152feb9
RefinedDev <aryantatee@gmail.com>
2021-07-29 16:09:12 +0530

    Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="EasierSQL",
-    version="0.0.2",
+    version="0.0.3",
     description="A wrapper for SQLite, Most of the queries wrapped into commands for ease.",
     long_description=long_description,
     long_description_content_type="text/markdown",
